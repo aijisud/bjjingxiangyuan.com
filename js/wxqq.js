d045c206448cc2553731ff54c8098aa1611acf33
SeanDu <aijisud@gmail.com>
2016-12-09 00:42:58 +0800

    All updates

---
+++ js/wxqq.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+var wxqq=document.getElementById("wxqq");
+
+wxqq.innerHTML += "<div class=\"wx\" style=\"position:fixed; left:0px; border:0px; z-index: 100; top: 180px;\"><img src=\"src/wx.jpg\" width=\"160\" border=\"0\" alt=\"扫码加微信\" title=\"扫码加微信\" /></div>";
+wxqq.innerHTML += "<div class=\"qq\" style=\"position:fixed; right:0px; border:0px; z-index: 100; top: 180px;\"><img src=\"src/qq.jpg\" width=\"160\" border=\"0\" alt=\"扫码加QQ\" title=\"扫码加QQ\" /></div>";
